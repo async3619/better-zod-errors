521267367a16ff2f5126854e3891d1cb1626be8a
Sophia (Turner) <me@sophia-dev.io>
2025-09-17 23:25:10 +0900

    ci(github): add workflows for deploy and code quality checking automation (#7)

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,10 @@
-const { createDefaultPreset } = require('ts-jest')
+import { createDefaultPreset } from 'ts-jest'
 
 const tsJestTransformCfg = createDefaultPreset().transform
 
 const config: import('jest').Config = {
   testEnvironment: 'node',
+  testMatch: ['**/*.spec.ts'],
   transform: {
     ...tsJestTransformCfg,
   },
